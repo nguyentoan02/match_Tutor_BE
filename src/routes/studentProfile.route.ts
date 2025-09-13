82fe2e6ec81a1d86c84c8570492a09d7b219e66f
bangd1nh <dinhbang121@gmail.com>
2025-09-13 09:59:05 +0700

    validate request

--- src/routes/studentProfile.route.ts
+++ src/routes/studentProfile.route.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import studentController from "../controllers/student.controller";
 import { validate } from "../middlewares/validation.middleware";
-import { createStudentProfileSchema } from "../schemas/StudentProfile.schema";
+import {
+   createStudentProfileSchema,
+   updateStudentProfileSchema,
+} from "../schemas/StudentProfile.schema";
 import { authenticate } from "../middlewares/auth.middleware";
 import { uploadSingle } from "../middlewares/upload.middleware";
 import { parseJsonFields } from "../middlewares/jsonfields.moddleware";
@@ -9,26 +12,27 @@ import { parseJsonFields } from "../middlewares/jsonfields.moddleware";
 const router = Router();
 
 router.post(
-    "/createProfile",
-    authenticate,
-    uploadSingle("avatar"),
-    parseJsonFields,
-    validate(createStudentProfileSchema),
-    studentController.createProfile
+   "/createProfile",
+   authenticate,
+   uploadSingle("avatar"),
+   parseJsonFields,
+   validate(createStudentProfileSchema),
+   studentController.createProfile
 );
 
 router.get(
-    "/readStudentProfile",
-    authenticate,
-    studentController.readUserProile
+   "/readStudentProfile",
+   authenticate,
+   studentController.readUserProile
 );
 
 router.put(
-    "/updateStudentProfile",
-    uploadSingle("avatar"),
-    parseJsonFields,
-    authenticate,
-    studentController.updateUserProfile
+   "/updateStudentProfile",
+   uploadSingle("avatar"),
+   parseJsonFields,
+   validate(updateStudentProfileSchema),
+   authenticate,
+   studentController.updateUserProfile
 );
 
 export default router;
--- src/schemas/StudentProfile.schema.ts
+++ src/schemas/StudentProfile.schema.ts
@@ -5,60 +5,66 @@ import { TIME_SLOT_VALUES } from "../types/enums/timeSlot.enum";
 import { GENDER_VALUES } from "../types/enums/gender.enum";
 
 const addressSchema = z.object({
-    city: z.string().min(1, "City is required"),
-    street: z.string().min(1, "Street is required"),
-    lat: z.number().optional(),
-    lng: z.number().optional(),
+   city: z.string().min(1, "City is required"),
+   street: z.string().min(1, "Street is required"),
+   lat: z.number().optional(),
+   lng: z.number().optional(),
 });
 
 const availabilitySchema = z.object({
-    dayOfWeek: z.number().int().min(0).max(7),
-    slots: z
-        .array(z.enum(TIME_SLOT_VALUES))
-        .min(1, "At least 1 slot is required"),
+   dayOfWeek: z.number().int().min(0).max(7),
+   slots: z
+      .array(z.enum(TIME_SLOT_VALUES))
+      .min(1, "At least 1 slot is required"),
 });
 
 // CREATE schema
 export const createStudentProfileSchema = z.object({
-    body: z.object({
-        name: z.string().max(50, "Name must be at most 50 characters"),
-        phone: z.string().regex(/^\d{10}$/, "Phone must be exactly 10 digits"),
-        avatarUrl: z.string().url("Avatar must be a valid URL").optional(),
-        gender: z.enum(GENDER_VALUES, { error: "Gender is required" }),
-        address: addressSchema,
-        subjectsInterested: z
-            .array(z.enum(SUBJECT_VALUES))
-            .min(1, "At least 1 subject is required"),
-        gradeLevel: z.enum(LEVEL_VALUES, {
-            error: "Grade level is required",
-        }),
-        bio: z.string().min(1, "Bio is required"),
-        learningGoals: z.string().min(1, "Learning goal is required"),
-        availability: z
-            .array(availabilitySchema)
-            .min(1, "At least 1 availability is required"),
-    }),
+   body: z.object({
+      name: z.string().max(50, "Name must be at most 50 characters"),
+      phone: z.string().regex(/^\d{10}$/, "Phone must be exactly 10 digits"),
+      avatarUrl: z.string().url("Avatar must be a valid URL").optional(),
+      gender: z.enum(GENDER_VALUES, { error: "Gender is required" }),
+      address: addressSchema,
+      subjectsInterested: z
+         .array(z.enum(SUBJECT_VALUES))
+         .min(1, "At least 1 subject is required"),
+      gradeLevel: z.enum(LEVEL_VALUES, {
+         error: "Grade level is required",
+      }),
+      bio: z.string().min(1, "Bio is required"),
+      learningGoals: z.string().min(1, "Learning goal is required"),
+      availability: z
+         .array(availabilitySchema)
+         .min(1, "At least 1 availability is required"),
+   }),
 });
 
 // UPDATE schema (tất cả optional, nhưng nếu có thì phải đúng định dạng)
 export const updateStudentProfileSchema = z.object({
-    body: z.object({
-        name: z.string().max(50, "Name must be at most 50 characters"),
-        phone: z.string().regex(/^\d{10}$/),
-        avatarUrl: z.string().url(),
-        gender: z.enum(GENDER_VALUES),
-        address: addressSchema,
-        subjectsInterested: z.array(z.enum(SUBJECT_VALUES)).min(1),
-        gradeLevel: z.enum(LEVEL_VALUES),
-        bio: z.string().min(1),
-        learningGoals: z.string().min(1),
-        availability: z.array(availabilitySchema).min(1),
-    }),
+   body: z.object({
+      name: z.string().max(50, "Name must be at most 50 characters"),
+      phone: z.string().regex(/^\d{10}$/, "Phone must be exactly 10 digits"),
+      avatarUrl: z.string().url("Avatar must be a valid URL").optional(),
+      gender: z.enum(GENDER_VALUES, { error: "Gender is required" }),
+      address: addressSchema,
+      subjectsInterested: z
+         .array(z.enum(SUBJECT_VALUES))
+         .min(1, "At least 1 subject is required"),
+      gradeLevel: z.enum(LEVEL_VALUES, {
+         error: "Grade level is required",
+      }),
+      bio: z.string().min(1, "Bio is required"),
+      learningGoals: z.string().min(1, "Learning goal is required"),
+      availability: z
+         .array(availabilitySchema)
+         .min(1, "At least 1 availability is required"),
+   }),
 });
 
 export type UpdateStudentProfileBody = z.infer<
-    typeof updateStudentProfileSchema
+   typeof updateStudentProfileSchema
 >["body"];
 export type CreateStudentProfileBody = z.infer<
-    typeof createStudentProfileSchema
+   typeof createStudentProfileSchema
 >["body"];
