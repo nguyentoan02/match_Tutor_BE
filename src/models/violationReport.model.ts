54e38e3e9fc6ed014540c39b74037e6419cd15f6
toannv0211 <nvtvlog234@gmail.com>
2025-11-23 10:21:46 +0700

    fix: fix model report for real case that user really learned that tutor so they can report

--- src/models/violationReport.model.ts
+++ src/models/violationReport.model.ts
@@ -15,11 +15,6 @@ const ViolationReportSchema: Schema<IViolationReport> = new Schema(
       },
       reporterId: { type: Schema.Types.ObjectId, ref: "User", required: true },
       reportedUserId: { type: Schema.Types.ObjectId, ref: "User" },
-      relatedReviewId: { type: Schema.Types.ObjectId, ref: "Review" },
-      relatedTeachingRequestId: {
-         type: Schema.Types.ObjectId,
-         ref: "TeachingRequest",
-      },
       reason: { type: String },
       evidenceFiles: [{ type: String }],
       status: {
